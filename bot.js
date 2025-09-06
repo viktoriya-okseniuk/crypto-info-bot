30cbecd617004b2c15c72f08b687c28c6a43d490
Viktoriya Okseniuk <okseniuk.vi@gmail.com>
2025-09-06 15:29:33 +0300

    est commit

--- bot.js
+++ bot.js
@@ -58,6 +58,8 @@ async function getTopCoins() {
     const res = await fetch(
       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1'
     );
+    console.log('getTopCoins')
+
     const data = await res.json();
     if (!Array.isArray(data)) {
       console.error('CoinGecko error:', data);
